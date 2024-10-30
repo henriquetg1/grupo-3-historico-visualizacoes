3000bb0b5dea31af3fa6825723b270274c9cc812
henriquetg1 <henriqueturco1@gmail.com>
2024-10-29 22:48:47 -0300

    feat: getter e setter no usuario (lombok nao funcionava)

--- aula-13/campeonato/src/main/java/br/insper/loja/usuario/model/Usuario.java
+++ aula-13/campeonato/src/main/java/br/insper/loja/usuario/model/Usuario.java
@@ -1,6 +1,7 @@
 package br.insper.loja.usuario.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.insper.loja.filme.model.Filme;
 import jakarta.persistence.*;
@@ -19,6 +20,29 @@ public class Usuario {
     private String email;
 
     // MANY TO SOMETHING???
-    private ArrayList<Filme> historico = new ArrayList<>();
+    private List<Filme> historico = new ArrayList<>();
 
+    public List<Filme> getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(List<Filme> historico) {
+        this.historico = historico;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
