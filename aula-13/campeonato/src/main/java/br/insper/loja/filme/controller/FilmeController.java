c504a8edcd0a928f1f9f1bafab5835590b30f8dc
henriquetg1 <henriqueturco1@gmail.com>
2024-10-29 22:55:16 -0300

    feat: FilmeController pronto

--- aula-13/campeonato/src/main/java/br/insper/loja/filme/controller/FilmeController.java
+++ aula-13/campeonato/src/main/java/br/insper/loja/filme/controller/FilmeController.java
@@ -1,20 +1,33 @@
 package br.insper.loja.filme.controller;
 
-import br.insper.loja.filme.repository.FilmeRepository;
-import br.insper.loja.partida.service.PartidaService;
+import br.insper.loja.filme.model.Filme;
 import br.insper.loja.filme.service.FilmeService;
+import br.insper.loja.usuario.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @RestController
+@RequestMapping("/filme")
 public class FilmeController {
 
     @Autowired
     private FilmeService filmeService;
 
-    @Autowired
-    private PartidaService partidaService;
+    @PostMapping
+    public Filme salvarFilme(@RequestBody Usuario usuario, @RequestBody Filme filme) {
+        return filmeService.salvarFilme(usuario, filme);
+    }
 
-    @Autowired
-    private FilmeRepository filmeRepository;
+    @GetMapping
+    public List<Filme> listarFilmes(@RequestBody Usuario usuario, @RequestParam(required = false) LocalDateTime data, @RequestParam(required = false) String nome, @RequestParam(required = false) String genero) {
+        return filmeService.listarFilmes(usuario, data, nome, genero);
+    }
+
+    @GetMapping("/resumo")
+    public String resumo(@RequestBody Usuario usuario) {
+        return filmeService.resumo(usuario);
+    }
 }
