dbbc81ca132f6051682a62769e5cffd8e3afc643
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-11-05 23:12:51 -0300

    achar usuario de outro url

--- campeonato/src/main/java/br/insper/loja/usuario/service/UsuarioService.java
+++ campeonato/src/main/java/br/insper/loja/usuario/service/UsuarioService.java
@@ -1,4 +1,33 @@
 package br.insper.loja.usuario.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
 public class UsuarioService {
+
+    public RetornarUsuarioDto getDataWithJwt(String url, String jwtToken){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + jwtToken);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<RetornarUsuarioDto> response = restTemplate.exchange(
+          "http://localhost/8080/usuario",
+                HttpMethod.GET,
+                entity,
+                RetornarUsuarioDto.class
+
+
+        );
+
+        return respose.getBody();
+
+    }
+
 }
